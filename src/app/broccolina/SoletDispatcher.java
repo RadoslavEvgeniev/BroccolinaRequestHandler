2ab4606ab94fa560f9b24b767a238ce530ea6f14
Radoslav Evgeniev <r.evgeniev.ivanov@gmail.com>
2018-06-22 13:47:56 +0300

    Broccolina Servlet Container: Finished SessionManagementService Class

--- src/app/broccolina/SoletDispatcher.java
+++ src/app/broccolina/SoletDispatcher.java
@@ -1,16 +1,116 @@
 package app.broccolina;
 
-import app.broccolina.solet.HttpSolet;
+import app.broccolina.solet.*;
+import app.broccolina.util.ApplicationLoadingService;
+import app.broccolina.util.SessionManagementService;
+import app.javache.api.RequestHandler;
+import app.javache.io.Reader;
+import app.javache.io.Writer;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class SoletDispatcher {
+public class SoletDispatcher implements RequestHandler {
 
-//    private final String serverRootFolderPath;
+    private final String serverRootFolderPath;
 
     private boolean hasIntercepted;
 
     private Map<String, HttpSolet> soletMap;
 
-//    private ApplicationLoadingService applicationLoadingService;
+    private ApplicationLoadingService applicationLoadingService;
+
+    private SessionManagementService sessionManagementService;
+
+    public SoletDispatcher(String serverRootFolderPath) {
+        this.serverRootFolderPath = serverRootFolderPath;
+        this.hasIntercepted = false;
+        this.applicationLoadingService = new ApplicationLoadingService();
+        this.sessionManagementService = new SessionManagementService();
+        this.initializeSoletMap();
+    }
+
+    private void initializeSoletMap() {
+        try {
+            this.soletMap = this.applicationLoadingService.loadApplications(serverRootFolderPath + "webapps" + File.separator);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream) {
+        try {
+            HttpSoletRequest request = new HttpSoletRequestImpl(new Reader().readAllLines(inputStream), inputStream);
+            HttpSoletResponse response = new HttpSoletResponseImpl(outputStream);
+
+            this.sessionManagementService.initSessionIfExistent(request);
+
+            HttpSolet soletObject = this.findSoletCandidate(request, response);
+
+            if (request.isResource() || soletObject == null) {
+                this.hasIntercepted = false;
+                return;
+            }
+
+            Class[] soletServiceMethodParameters = Arrays
+                    .stream(soletObject.getClass().getMethods())
+                    .filter(m -> m.getName().equals("service"))
+                    .findFirst()
+                    .orElse(null)
+                    .getParameterTypes();
+
+            soletObject.getClass()
+                    .getMethod("service", soletServiceMethodParameters[0], soletServiceMethodParameters[1])
+                    .invoke(soletObject, request, response);
+
+            this.sessionManagementService.sendSessionIfExistent(request, response);
+
+            this.sessionManagementService.clearInvalidSessions();
+
+            new Writer().writeBytes(response.getBytes(), response.getOutputStream());
+
+            this.hasIntercepted = true;
+        } catch (IOException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+            this.hasIntercepted = false;
+        }
+    }
+
+    private HttpSolet findSoletCandidate(HttpSoletRequest request, HttpSoletResponse response) {
+        String reuestUrl = request.getRequestUrl();
+
+        Pattern applicationRouteMatchPattern = Pattern.compile("/[a-zA-Z0-9]+/");
+        Matcher applicationRouteMatcher = applicationRouteMatchPattern.matcher(reuestUrl);
+
+        if (this.soletMap.containsKey(reuestUrl)) {
+            return this.soletMap.get(reuestUrl);
+        }
+
+        if (applicationRouteMatcher.find()) {
+            String applicationRoute = applicationRouteMatcher.group(0) + "*";
+
+            if (this.soletMap.containsKey(applicationRoute)) {
+                return this.soletMap.get(applicationRoute);
+            }
+        }
+
+        if (this.soletMap.containsKey("/*")) {
+            return this.soletMap.get("/*");
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean hasIntercepted() {
+        return this.hasIntercepted;
+    }
 }
