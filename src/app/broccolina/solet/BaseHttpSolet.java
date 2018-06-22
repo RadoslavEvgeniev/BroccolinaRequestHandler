2d036f012ae43c8129b770b99f33a7f80288ad2a
Radoslav Evgeniev <r.evgeniev.ivanov@gmail.com>
2018-06-22 11:07:52 +0300

    Broccolina Request Handler: BaseHttpSoletFix

--- src/app/broccolina/solet/BaseHttpSolet.java
+++ src/app/broccolina/solet/BaseHttpSolet.java
@@ -1,7 +1,6 @@
 package app.broccolina.solet;
 
 import app.javache.http.HttpStatus;
-import app.javache.io.Writer;
 
 import java.io.IOException;
 
@@ -15,14 +14,37 @@ public abstract class BaseHttpSolet implements HttpSolet {
         this.isInitialized = false;
     }
 
-    private void configureNotFound(HttpSoletResponse response) {
+    private void configureNotFound(HttpSoletRequest request, HttpSoletResponse response) {
         response.setStatusCode(HttpStatus.NOT_FOUND);
 
         response.addHeader("Content-Type", "text/html");
     }
 
+    protected void doGet(HttpSoletRequest request, HttpSoletResponse response) {
+        this.configureNotFound(request, response);
+
+        response.setContent(("<h1>[ERROR] GET " + request.getRequestUrl() + "</h1><br/><h3>[MESSAGE] The page or functionality you are looking for is not found. </h3>").getBytes());
+    }
+
+    protected void doPost(HttpSoletRequest request, HttpSoletResponse response) {
+        this.configureNotFound(request, response);
+
+        response.setContent(("<h1>[ERROR] POST " + request.getRequestUrl() + "</h1><br/><h3>[MESSAGE] The page or functionality you are looking for is not found. </h3>").getBytes());
+    }
+
+    protected void doPut(HttpSoletRequest request, HttpSoletResponse response) {
+        this.configureNotFound(request, response);
+        response.setContent(("<h1>[ERROR] PUT " + request.getRequestUrl() + "</h1><br/><h3>[MESSAGE] The page or functionality you are looking for is not found. </h3>").getBytes());
+    }
+
+    protected void doDelete(HttpSoletRequest request, HttpSoletResponse response) {
+        this.configureNotFound(request, response);
+        response.setContent(("<h1>[ERROR] DELETE " + request.getRequestUrl() + "</h1><br/><h3>[MESSAGE] The page or functionality you are looking for is not found. </h3>").getBytes());
+    }
+
     @Override
     public void init(SoletConfig soletConfig) {
+        this.isInitialized = true;
         this.soletConfig = soletConfig;
     }
 
@@ -42,27 +64,7 @@ public abstract class BaseHttpSolet implements HttpSolet {
         } else if (request.getMethod().equals("DELETE")) {
             this.doDelete(request, response);
         }
-
-        Writer.writeBytes(response.getBytes(), response.getOutputStream());
     }
 
-    protected void doGet(HttpSoletRequest request, HttpSoletResponse response) {
-        this.configureNotFound(response);
-        response.setContent(("[ERROR] GET " + request.getRequestUrl() + " The page or functionality you are looking for is not found.").getBytes());
-    }
 
-    protected void doPost(HttpSoletRequest request, HttpSoletResponse response) {
-        this.configureNotFound(response);
-        response.setContent(("[ERROR] POST " + request.getRequestUrl() + " The page or functionality you are looking for is not found.").getBytes());
-    }
-
-    protected void doPut(HttpSoletRequest request, HttpSoletResponse response) {
-        this.configureNotFound(response);
-        response.setContent(("[ERROR] PUT " + request.getRequestUrl() + " The page or functionality you are looking for is not found.").getBytes());
-    }
-
-    protected void doDelete(HttpSoletRequest request, HttpSoletResponse response) {
-        this.configureNotFound(response);
-        response.setContent(("[ERROR] DELETE " + request.getRequestUrl() + " The page or functionality you are looking for is not found.").getBytes());
-    }
 }
