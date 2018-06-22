87e1655f7425b2686ffc9b667a62f85664a7c40c
Radoslav Evgeniev <r.evgeniev.ivanov@gmail.com>
2018-06-22 11:30:25 +0300

    Broccolina Request Handler: Added WebSolet annotation

---
+++ src/app/broccolina/solet/WebSolet.java
@@ -0,0 +1,15 @@
+package app.broccolina.solet;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface WebSolet {
+
+    String route() default  "";
+
+    boolean loadOnStartUp() default true;
+}
